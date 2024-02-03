8c656a6499dc1bea0eebbd456cd45252476aa56b
Cameron <you@example.com>
2024-02-03 07:04:35 -0800

    V2

--- gensite.sh
+++ gensite.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "What shall we name this web site good sir?" 
+echo "What shall we name this web site good sir?"
 read dirname
-for gen in $dirname/index.html $dirname/style.css $dirname/script.js; do [ ! -d `dirname $dirname` ] || mkdir -p $dirname; touch $gen;done 
+for gen in $dirname/index.html $dirname/style.css $dirname/script.js; do
+    [ ! -d $(dirname $dirname) ] || mkdir -p $dirname
+    touch $gen
+done
+echo "<!DOCTYPE html>
+<html>
+<head>
+    <title>Your HTML Template</title>
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+    <script src=\"script.js\"></script>
+</head>
+p
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is your HTML template.</p>
+</body>
+</html>" >"$dirname/index.html"
